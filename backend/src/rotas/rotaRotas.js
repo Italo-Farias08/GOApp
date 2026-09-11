8dc89107f1ae05157a7b4f22b7d83e4a4f919abd
Italo Farias <italo.farias1wi@gmail.com>
2026-09-11 02:34:53 -0300

    gh

--- backend/src/rotas/rotaRotas.js
+++ backend/src/rotas/rotaRotas.js
@@ -1,11 +1,12 @@
 const express = require('express');
-const saqueControlador = require('../controladores/saqueControlador');
+const rotaControlador = require('../controladores/rotaControlador');
 const autenticacaoIntermediario = require('../intermediarios/autenticacaoIntermediario');
 
 const roteador = express.Router();
 
 roteador.use(autenticacaoIntermediario);
 
-roteador.post('/sacar', saqueControlador.solicitar);
+// GET /routing/rota?origemLat=...&origemLng=...&destinoLat=...&destinoLng=...
+roteador.get('/rota', rotaControlador.calcularRota);
 
 module.exports = roteador;
